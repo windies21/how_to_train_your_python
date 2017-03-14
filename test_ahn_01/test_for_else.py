9077dbbf0875e0d285080cc3be128a0bfcdc2d85
gluebox <ahn@DI1702NO01.local>
2017-03-14 11:49:19 +0900

    과제 01 수정

--- test_ahn_01/test_for_else.py
+++ test_ahn_01/test_for_else.py
@@ -3,10 +3,13 @@ import unittest
 
 class TestForElse(unittest.TestCase):
     def test_for_else(self):
+        # for else 문의 발견
+
         # GIVEN
         m = 3
         n = 100
 
+        """ for else 문을 이해하기 위해 강제로 같은 결과값이 나오는 루프 조작. """
         a = set()
         for num in range(m, n + 1):
             for i in range(2, num):
